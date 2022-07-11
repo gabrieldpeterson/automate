ec0459d680402b73338024a46650fe0226e56a30
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-10 19:29:11 -0700

    Add verification that the rest of the code went through

--- automate.py
+++ automate.py
@@ -8,10 +8,15 @@ driver.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 driver.maximize_window()
 
 message_input = driver.find_element(by=By.ID, value='user-message')
+message_input.clear()
 message_input.send_keys('test text')
 
 message_button = driver.find_element(by=By.XPATH, value='//button[normalize-space()="Show Message"]')
 message_button.click()
+
+output_message = driver.find_element(by=By.ID, value='display')
+
+assert 'test text' in output_message.text
 
 # Don't kill browser immediately after running
 while True:
